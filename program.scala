64b99e3d0399e1325dfdb83c221e04d03b66fd46
Ash Pook <sungiant@live.co.uk>
2019-01-21 19:58:51 +0000

    Clearer scene specification.

--- program.scala
+++ program.scala
@@ -4,6 +4,27 @@ object Extensions { implicit class I[A](val self: A) { def |>[B](f: A => B): B =
 object Configuration { lazy val tolerance = 0.0000001 }; import Configuration._
 
 //--------------------------------------------------------------------------------------------------------------------//
+
+case class Quaternion (i: Double, j: Double, k: Double, u: Double)
+object Quaternion {
+  lazy val identity = Quaternion (0, 0, 0, 1)
+
+  // Right handed co-ordinate system.
+  def fromYawPitchRoll (yaw: Double, pitch: Double, roll: Double): Quaternion = {
+    val (y, p, r) = (yaw * 0.5, pitch * 0.5, roll * 0.5)
+    val (sy, cy) = (y |> Math.sin, y |> Math.cos)
+    val (sp, cp) = (p |> Math.sin, p |> Math.cos)
+    val (sr, cr) = (r |> Math.sin, r |> Math.cos)
+    Quaternion (
+      (cy * sp * cr) + (sy * cp * sr),
+      (sy * cp * cr) - (cy * sp * sr),
+      (cy * cp * sr) - (sy * sp * cr),
+      (cy * cp * cr) + (sy * sp * sr))
+  }
+}
+
+case class Ray (position: Vector, direction: Vector)
+
 case class Vector (x: Double, y: Double, z: Double) {
   lazy val isZero: Boolean = Math.abs (x) < tolerance && Math.abs (y) < tolerance && Math.abs (z) < tolerance
   lazy val length: Double = isZero match { case false => (x * x + y * y + z * z) |> Math.sqrt; case true => 0.0 }
@@ -52,119 +73,109 @@ object Vector {
     a.z + (b.z - a.z) * f.z)
 }
 
-//--------------------------------------------------------------------------------------------------------------------//
-case class Quaternion (i: Double, j: Double, k: Double, u: Double)
-object Quaternion {
-  lazy val identity = Quaternion (0, 0, 0, 1)
-
-  def fromYawPitchRoll (yaw: Double, pitch: Double, roll: Double): Quaternion = {
-    val (y, p, r) = (yaw * 0.5, pitch * 0.5, roll * 0.5)
-    val (sy, cy) = (y |> Math.sin, y |> Math.cos)
-    val (sp, cp) = (p |> Math.sin, p |> Math.cos)
-    val (sr, cr) = (r |> Math.sin, r |> Math.cos)
-    Quaternion (
-      (cy * sp * cr) + (sy * cp * sr),
-      (sy * cp * cr) - (cy * sp * sr),
-      (cy * cp * sr) - (sy * sp * cr),
-      (cy * cp * cr) + (sy * sp * sr))
-  }
-}
-
-//--------------------------------------------------------------------------------------------------------------------//
-case class Colour (r: Char, g: Char, b: Char)
+case class Colour (r: Byte, g: Byte, b: Byte)
 object Colour {
-  lazy val black = Colour (0.toChar, 0.toChar, 0.toChar)
-  lazy val white = Colour (255.toChar, 255.toChar, 255.toChar)
+  lazy val black = Colour (0.toByte, 0.toByte, 0.toByte)
+  lazy val white = Colour (255.toByte, 255.toByte, 255.toByte)
 
   def fromNormal (v: Vector): Colour = Colour (
-    ((v.x + 1.0) / 2.0 * 255.0).toChar,
-    ((v.y + 1.0) / 2.0 * 255.0).toChar,
-    ((v.z + 1.0) / 2.0 * 255.0).toChar)
+    ((v.x + 1.0) / 2.0 * 255.0).toByte,
+    ((v.y + 1.0) / 2.0 * 255.0).toByte,
+    ((v.z + 1.0) / 2.0 * 255.0).toByte)
 }
 
-//--------------------------------------------------------------------------------------------------------------------//
-case class Ray (position: Vector, direction: Vector)
-
-//--------------------------------------------------------------------------------------------------------------------//
-// `fov` is the vertical field of view; horizontal FOV varies depending on the viewport's aspect ratio
-case class Camera (position: Vector, orientation: Quaternion, fov: Double, aspectRation: Double, near: Double, far: Double) {
-  def getRay (x: Double, y: Double): Ray = {
+// Camera `fov` is the vertical field of view; horizontal FOV varies depending on the viewport's aspect ratio
+case class Camera (position: Vector, orientation: Quaternion, fov: Double, aspect: Double, near: Double, far: Double) {
+  // Given a location anywhere on the near bounding surface of the camera's view frustum, returns the associated ray.
+  def ray (x: Double, y: Double): Ray = {
     val xx = (x + 1.0) / 2.0
     val yy = (y + 1.0) / 2.0
     val sh = Math.sin (fov / 2.0)
-    val nearNW: Vector = Vector ( near * sh * aspectRation,  near * sh, near)
-    val nearSE: Vector = Vector (-near * sh * aspectRation, -near * sh, near)
-    val farNW:  Vector = Vector (  far * sh * aspectRation,   far * sh, far)
-    val farSE:  Vector = Vector (- far * sh * aspectRation, - far * sh, far)
+    val nnw = Vector ( near * sh * aspect,  near * sh, near)
+    val nse = Vector (-near * sh * aspect, -near * sh, near)
+    val fnw = Vector (  far * sh * aspect,   far * sh, far)
+    val fsw = Vector (- far * sh * aspect, - far * sh, far)
     val f = Vector (xx, yy, 0.0)
-    val p = position + Vector.lerp (nearNW, nearSE, f).transform (orientation)
-    val d = Vector.lerp (farNW - nearNW, farSE - nearSE, f).transform (orientation).normalise
+    val p = position + Vector.lerp (nnw, nse, f).transform (orientation)
+    val d = Vector.lerp (fnw - nnw, fsw - nse, f).transform (orientation).normalise
     Ray (p, d)
   }
 }
 
-//--------------------------------------------------------------------------------------------------------------------//
-// Simple class for writting PPM files to disk.
-case class Image (width: Int, height: Int, pixels: List[Colour]) {
-  def getPixel (x: Int, y: Int): Colour = pixels (x + y * width)
-  def pixelTable: List[(Int, Int)] = (0 until height).toList.flatMap { y => (0 until width).toList.map { x => (x, y) } }
+case class Image (width: Int, height: Int, pixels: IndexedSeq[Colour]) {
+  def get (x: Int, y: Int): Colour = pixels (x + y * width)
 }
 object Image {
-  def create (w: Int, h: Int)(r: List[Colour]): Image = Image (w, h, r)
+  def create (w: Int, h: Int)(r: IndexedSeq[Colour]): Image = Image (w, h, r)
   def toNetPBM (path: String)(image: Image): Unit = {
     val out = new java.io.DataOutputStream (new java.io.FileOutputStream (path))
     out.writeBytes ("P6\u000a%d %d\u000a%d\u000a".format (image.width, image.height, 255))
-    image.pixelTable
-      .map { case (x, y) => image.getPixel (x, y) }
-      .foreach { c =>
-        out.writeByte (c.r)
-        out.writeByte (c.g)
-        out.writeByte (c.b)
-      }
+    (0 until image.height)
+      .flatMap { y => (0 until image.width).map { x => (x, y) } }
+      .map { case (x, y) => image.get (x, y) }
+      .foreach { c => out.writeByte (c.r); out.writeByte (c.g); out.writeByte (c.b) }
   }
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
-// The absolute value of the return value indicates the distance to the surface.
-// The sign indicates whether the position is inside or outside the surface, negative indicating inside.
-object SDF {
-  def intersect (a: Double, b: Double): Double = Math.max (a, b)
-  def union (a: Double, b: Double): Double = Math.min (a, b)
-  def difference (a: Double, b: Double): Double = Math.max (a, -b)
 
-  // unit sphere (diameter = 1)
-  def sphere (position: Vector, offset: Vector, diameter: Double): Double = 
-    (position - offset).length - (diameter / 2.0)
+object Program {
 
-  // unit cube with (h,w,d = 1)
-  def cube (position: Vector, offset: Vector, size: Double): Double = {
+  // Defines the signature of a signed distance function.  The absolute value of the result indicates the distance to
+  // the surface, the sign of the result indicates whether the position is inside or outside of the surface, negative indicating inside.
+  type SDF = Vector => Double
+
+  // Enumeration of constructive solid geometry operations
+  trait CSG; object CSG { object Intersect extends CSG; object Union extends CSG; object Difference extends CSG }
+
+  // A scene is defined as a list of edits.
+  case class Edit (sdf: SDF, op: CSG)
+
+  // Folds a list of edits into an SDF.
+  def evaluate (edits: List[Edit]): Option[SDF] = edits.foldLeft (None: Option[SDF]){ (acc: Option[SDF], edit: Edit) =>
+    Some { (pos: Vector) => 
+      val b = edit.sdf (pos)
+      acc match {
+        case None => b // Ignore the first edit's op.
+        case Some (asdf) =>
+          val a = asdf (pos)
+          edit.op match {
+            case CSG.Intersect   => Math.max ( a,  b)
+            case CSG.Union       => Math.min ( a,  b)
+            case CSG.Difference  => Math.max ( a, -b)
+          }
+      }
+    }
+  }
+
+  // Signed distance function for a unit sphere (diameter = 1).
+  def sphere (offset: Vector, diameter: Double)(position: Vector): Double = (position - offset).length - (diameter / 2.0)
+
+  // Signed distance function for a unit cube (h,w,d = 1).
+  def cube (offset: Vector, size: Double)(position: Vector): Double = {
     val d: Vector = (position - offset).abs - (Vector (size, size, size) / 2.0)
     val insideDistance: Double = Math.min (Math.max (d.x, Math.max (d.y, d.z)), 0.0)
     val outsideDistance: Double = d.max (0.0).length
     insideDistance + outsideDistance
   }
-}
-
-//--------------------------------------------------------------------------------------------------------------------//
-// Where it all kicks off.  Right handed co-ordinate system.
-object Program {
   
-  def distanceAlongRayWhenItIntersectsASurface (ray: Ray, sdf: (Vector) => Double, start: Double, end: Double): Option[Double] = {
+  // Given a ray and an SDF recursively evaluates the SDF until an interestion is either found or the limit of iterations is reached.
+  def distanceAlongRayWhenItHitsASurface (ray: Ray, sdf: (Vector) => Double, start: Double, end: Double): Option[Double] = {
+    val limit = 100
     @scala.annotation.tailrec
     def r (depth: Double, step: Int): Option[Double] = step match {
-      case 0 => None
+      case 0.0 => None
       case _ =>
-        val pos = ray.position + ray.direction * depth
-        (pos |> sdf) match {
-          case dist if dist < tolerance => Some (depth) // hit, p is inside the surface
+        val p = ray.position + ray.direction * depth
+        (p |> sdf) match {
+          case dist if dist < tolerance => Some (depth) // Hit! `p` is inside the surface.
           case dist =>
             val newDepth = depth + dist
             if (newDepth >= end) None
             else r (newDepth, step - 1)
         }
     }
-    val maxSteps = 100
-    r (start, maxSteps)
+    r (start, step = limit)
   }
 
   def estimateNormal (pos: Vector, sdf: (Vector) => Double): Vector = {
@@ -176,17 +187,12 @@ object Program {
     ).normalise
   }
 
-  def scene (pos: Vector): Double = {
-    SDF.difference (
-      SDF.difference (
-        SDF.difference (
-          SDF.intersect (
-            SDF.cube (pos, Vector.zero, 2.0),
-            SDF.sphere (pos, Vector.zero, 2.4)),
-          SDF.sphere (pos, Vector.unitX, 0.8)),
-        SDF.sphere (pos, Vector.unitY, 0.8)),
-      SDF.sphere (pos, Vector.unitZ, 0.8))
-  }
+  val scene: List[Edit] =
+    Edit (  cube (Vector.zero,  2.0) _, CSG.Intersect ) ::
+    Edit (sphere (Vector.zero,  2.4) _, CSG.Intersect ) ::
+    Edit (sphere (Vector.unitX, 0.8) _, CSG.Difference) ::
+    Edit (sphere (Vector.unitY, 0.8) _, CSG.Difference) ::
+    Edit (sphere (Vector.unitZ, 0.8) _, CSG.Difference) :: Nil
 
   def demo (w: Int, h: Int): Image = { // w & h in units of pixels
     val (y, zx) = (4.0, 6.0)
@@ -195,16 +201,18 @@ object Program {
       Vector (zx, y, zx),
       Quaternion.fromYawPitchRoll (5.0 * math.Pi / 4.0, (math.Pi / 2.0) - theta, 0.0),
       57.0, w.toDouble / h.toDouble, 0.1, 100.0)
-    (0 until h).toList.flatMap { j =>
-      (0 until w).toList.map { i =>
+    (0 until h).flatMap { j =>
+      (0 until w).map { i =>
         val fx = 2.0 - (2.0 * (i.toDouble + 0.5) / w.toDouble) - 1.0 // x axis: -1.0 to 1.0
         val fy = 2.0 - (2.0 * (j.toDouble + 0.5) / h.toDouble) - 1.0 // y axis: -1.0 to 1.0
-        val pixelRay = camera.getRay (fx, fy)
-        distanceAlongRayWhenItIntersectsASurface (pixelRay, scene, camera.near, camera.far) match {
+        val pixelRay = camera.ray (fx, fy)
+
+        val sdf = evaluate (scene).getOrElse ((_: Vector) => 0.0)
+        distanceAlongRayWhenItHitsASurface (pixelRay, sdf, camera.near, camera.far) match {
           case None => Colour.black
           case Some (d) =>
             val pointOfIntersection = pixelRay.position + pixelRay.direction * d
-            val normal = estimateNormal (pointOfIntersection, scene)
+            val normal = estimateNormal (pointOfIntersection, sdf)
             Colour.fromNormal (normal)
         }
       }
